33acf9aaf3b7e5bb228a981cf6dd2580efdc03c0
Dams-d <damien.duro8@gmail.com>
2021-12-16 09:59:29 +0100

    Auht0 update callbacks with admin api

--- src/graphql.js
+++ src/graphql.js
@@ -8,3 +8,12 @@ export const GET_AUTH0_ROLES = gql`
         }
     }
 `;
+
+export const UPDATE_AUTH0_CLIENT = gql`
+    mutation UpdateAuth0Client($designId: String!, $settingsId: String!, $clientId: String!, $data: JSON!) {
+        updateAuth0Client(designId: $designId, settingsId: $settingsId, clientId: $clientId, data: $data) {
+            success
+            data
+        }
+    }
+`;
